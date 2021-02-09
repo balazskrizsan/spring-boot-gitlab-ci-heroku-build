b9d6842670b3d135c55f24bf7dc7193eb4e9c5c0
Balazs Krizsan <krizsan.balazs@gmail.com>
2021-02-09 19:34:21 +0100

    init

---
+++ src/main/java/com/kbalazsworks/build/BuildApplication.java
@@ -0,0 +1,26 @@
+package com.kbalazsworks.build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BuildApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BuildApplication.class, args);
+    }
+
+    @RestController
+    public class TestController
+    {
+        @GetMapping(value = "/")
+        public String hello()
+        {
+            return "Hello World!!";
+        }
+    }
+}
